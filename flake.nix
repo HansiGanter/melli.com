02626d6102fb454cbc85a5f393bc3fc72b21c2f2
Felix Andreas <felix.andreas@mit-emilia.de>
2022-08-23 18:44:41 +0200

    chore: Add container with `code-server` and `pnpm` (#70)

--- flake.nix
+++ flake.nix
@@ -4,9 +4,11 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    nix2container.url = "github:nlewo/nix2container";
+    nix2container.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, nix2container }:
     flake-utils.lib.eachDefaultSystem (system:
       with nixpkgs.legacyPackages.${system};
       {
@@ -17,6 +19,24 @@
             nodePackages.pnpm
           ];
         };
+        packages.container =
+          with nix2container.packages.x86_64-linux.nix2container;
+          buildImage
+            {
+              name = "docker.meetap.de/melli.com";
+              tag = "latest";
+              copyToRoot = [
+                (pkgs.buildEnv {
+                  name = "root";
+                  paths = [ code-server nodePackages.pnpm ];
+                  pathsToLink = [ "/bin" ];
+                })
+              ];
+              config = {
+                Cmd = [ "/bin/code-server" "--bind-addr" "0.0.0.0:8080" ];
+              };
+            };
       }
     );
 }
+
