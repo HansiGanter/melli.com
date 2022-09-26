128a8576d2768fd37c82cb8b43292593c3be8475
Hans Ganter <82868427+HansiGanter@users.noreply.github.com>
2022-09-26 12:53:20 +0200

    feat: added video-urls (#204)
    closes #203

---
+++ pages/video.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+    const route=useRoute()
+</script>
+
+<template>
+  <Container class="py-5 lg:p-5">
+    <div class="mx-auto relative overflow-hidden rounded-2xl h-fit w-fit">
+      <video
+        width="845"
+        height="475"
+        autoplay
+        controls
+        loop
+      >
+        <source
+          :src="`https://videos.melli.com/${route.query.video_name}`"
+          type="video/mp4"
+        >
+      </video>
+    </div>
+  </Container>
+</template>
