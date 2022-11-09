2a7ad85cba069615ff6eac42c048336e94f8e3c1
Hans Ganter <82868427+HansiGanter@users.noreply.github.com>
2022-11-09 16:34:07 +0100

    Refactor secure section (#312)
    closes #295 closes #301
    
    Co-authored-by: Navdha Jain <navdha.jain@melli.com>

--- src/components/LandingPage/SecureSection.vue
+++ src/components/LandingPage/SecureSection.vue
@@ -3,18 +3,22 @@ const { t } = useI18n()
 </script>
 
 <template>
-  <div class="bg-tertiary-100 grid gap-16 pb-[7%] sm:p-[7%] rounded-[32px] overflow-hidden mx-auto md:max-w-3/4 lg:max-w-2/3">
-    <img src="https://assets.melli.com/images/stock/melli_mockup-print_brochure2-1024.webp" class="sm:rounded-[32px]">
-    <div class="grid gap-4 lg:gap-8 text-center mx-6">
-      <h1 class="text-gray-900 font-semibold text-3xl lg:text-5xl">
-        {{ t('secure-melli.heading') }}
-      </h1>
-      <p class="text-gray-900 font-normal text-xl lg:text-2xl">
-        {{ t('secure-melli.sub-text') }}
-      </p>
+  <div class="bg-tertiary-100 grid md:grid-cols-2 rounded-[32px] overflow-hidden">
+    <div class="sm:rounded-[32px] md:rounded-none justify-center">
+      <img src="https://assets.melli.com/images/stock/melli_mockup-print_brochure2-1024.webp" class="object-cover h-full">
     </div>
-    <div class="grid gap-2 md:w-3/4 lg:w-2/3 mb-8 mx-6 lg:mb-8 md:mx-auto">
-      <NewsletterOpen />
+    <div class="grid gap-10 px-4 sm:px-16 py-8 md:px-4 lg:px-12 lg:py-24">
+      <div class="grid gap-4 text-center mx-6">
+        <h1 class="text-gray-900 font-semibold text-3xl lg:text-4xl">
+          {{ t('secure-melli.heading') }}
+        </h1>
+        <p class="text-gray-900 font-normal text-xl">
+          {{ t('secure-melli.sub-text') }}
+        </p>
+      </div>
+      <span>
+        <NewsletterOpen />
+      </span>
     </div>
   </div>
 </template>
