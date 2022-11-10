090305f076f67213e1e5b7b98e08cd049eec3155
Vokksy <68820713+Vokksy@users.noreply.github.com>
2022-11-10 16:23:20 +0100

    feat: add cookie banner to landing page (#305)
    close #296
    
    Co-authored-by: Hans Ganter <82868427+HansiGanter@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import generatedRoutes from '~pages'
 
 import '@unocss/reset/tailwind.css'
 import './styles/main.css'
+import './styles/cookie.css'
 import 'uno.css'
 
 const routes = setupLayouts(generatedRoutes)
