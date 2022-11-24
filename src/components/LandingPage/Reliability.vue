5f72c1b8cfa9f541267dcd3a094d4688d5bef493
mit-navdha <108458070+mit-navdha@users.noreply.github.com>
2022-11-24 19:28:30 +0100

    feat: elements on the landing page clickable (#342) (#343)
    #342

--- src/components/LandingPage/Reliability.vue
+++ src/components/LandingPage/Reliability.vue
@@ -3,6 +3,9 @@ import { fireVideoEvent } from '~/google-tag-manager'
 const { t } = useI18n()
 
 const features = ref([t('feature-1'), t('feature-2'), t('feature-3')])
+const showInformation = ref(-1)
+const textToShow = [t('reliability-section.card1'), t('reliability-section.card2'), t('reliability-section.card3')]
+const iconsSources = ['https://assets.melli.com/bubble-icons/bubble-icon_users-thick.svg', 'https://assets.melli.com/bubble-icons/bubble-icon_activity-thick.svg', 'https://assets.melli.com/bubble-icons/bubble-icon_phone-call-thick.svg']
 
 const reliabilityvideo = ref()
 const isPlaying = ref(false)
@@ -24,13 +27,14 @@ const openVideo = () => {
 </script>
 
 <template>
+  <!-- Add green color here if necessary -->
   <div class="grid gap-8 lg:gap-16 mx-auto items-center">
     <div class="grid gap-4 lg:gap-6 sm:text-center max-w-2xl mx-auto justify-items-center">
       <h1 class="text-gray-900 font-semibold text-4xl lg:text-5xl">
         {{ t('reliability-section.heading') }}
       </h1>
     </div>
-    <div class="grid gap-16 order-2 lg:order-1  bg-primary-100 px-6 lg:px-16 pt-10 lg:pt-16 pb-16 lg:pb-96">
+    <div class="grid gap-8 order-2 lg:order-1  bg-primary-100 px-6 lg:px-16 pt-10 lg:pt-16 pb-16 lg:pb-96">
       <div class="grid gap-14 w-full lg:w-2/3">
         <p class="w-full lg:w-3/4 font-normal text-xl">
           {{ t('reliability-section.text') }}
@@ -40,13 +44,35 @@ const openVideo = () => {
         <div
           v-for="(feature, index) in features"
           :key="index"
-          class="text-gray-900 font-semibold text-base bg-primary-900/10 w-full sm:w-fit py-3.5 px-6 rounded-lg lg:h-full flex items-center"
+          class="text-gray-900 font-semibold text-base bg-primary-900/10 w-full sm:w-fit py-3.5 px-4 rounded-lg lg:h-full flex items-center"
         >
-          <span>{{ feature }}</span>
+          <ul class="p-0" @click="showInformation = index">
+            <li class="flex gap-2 mx-auto items-center">
+              <img :src="iconsSources[index]" class="flex justify-center w-9 h-9 pr-1 shrink-0 object-fit object-center">
+              <div class="text-base">
+                {{ feature }}
+              </div>
+            </li>
+          </ul>
+
+          <Modal :show="showInformation === index" @close="showInformation = -1">
+            <Container class="p-6 max-w-3xl">
+              <img :src="iconsSources[index]" class="color-primary-900 w-14 h-14 w-full md:w-auto">
+              <div class="font-bold pt-4 py-2 text-xl">
+                {{ feature }}
+              </div>
+              <div class="text-xl">
+                {{ textToShow[index] }}
+              </div>
+            </Container>
+          </Modal>
         </div>
       </div>
     </div>
-    <div class="relative order-1 lg:order-2 overflow-hidden lg:max-w-6xl rounded-2xl lg:-mt-88 mx-auto" @click="openVideo">
+    <div
+      class="relative order-1 lg:order-2 overflow-hidden lg:max-w-6xl rounded-2xl lg:-mt-88 mx-auto"
+      @click="openVideo"
+    >
       <video
         ref="reliabilityvideo"
         width="1024"
