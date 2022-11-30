a9400364b1d7a79a6d761f2ea28f1310d2d3fa53
Hans Ganter <hansi.ganter@gmx.at>
2022-11-30 15:26:23 +0100

    fix job advertisement career page

--- pages/einwilligungserklaerung-dsgvo2.md
+++ pages/einwilligungserklaerung-dsgvo2.md
@@ -1,5 +1,7 @@
 ---
 title: Einwilligungserklärung
+category:
+    href: legal
 ---
 
 Ich bin damit einverstanden, dass die von mir angegebenen personenbezogenen Daten (insbesondere Name, Emailadresse und Telefonnummer) durch die meetap GmbH, Manfred-von-Richthofen-Str. 4, 12101 Berlin, für Werbezwecke (Produktinformationen zu Melli sowie Produktneuheiten, Rabattaktionen, Freunde-werben-Freunde Aktionen, Angeboten zum Abschluss eines Abonnements, Blogartikeln, Events rund um das Thema Melli und sprachgesteuerte Alltagsbegleiter für Senioren) sowie für Kundenzufriedenheitsumfragen verwendet werden können. Die erlaubte Nutzung zu Werbezwecken umfasst die Kontaktaufnahme per WhatsApp, SMS, Email, Telefon, App-Benachrichtigungen oder durch sonstige Formen der elektronischen Post.
--- src/components/MarkdownWrapper.vue
+++ src/components/MarkdownWrapper.vue
@@ -29,35 +29,26 @@ if (props.frontmatter.title) {
 <template>
   <!-- we wrap pages -->
   <div v-if="frontmatter.title">
+    <!-- Blogs -->
     <div v-if="frontmatter.category && frontmatter.category.href === 'blogs'">
       <ArticleHeader v-if="frontmatter.title" :hero-url="frontmatter.imageUrl" :category="frontmatter.category.name" :heading="frontmatter.title" :description="frontmatter.description" />
       <ArticleAuthor v-if="frontmatter.author" :author="frontmatter.author.name" :date="d(frontmatter.date, 'long')" />
-      <div v-if="frontmatter.category && frontmatter.category.href === 'jobs'">
-        <KarriereHeader :title="frontmatter.title" :period="frontmatter.period" />
-      </div>
-      <!-- <header v-else class="m-auto text-center prose mb-12">
-        <h1>{{ frontmatter.title }}</h1>
-        <p class="text-lg">
-          {{ frontmatter.description }}
-        </p>
-        <div class="text-lg opacity-50">
-          <span v-if="frontmatter.date">{{ d(frontmatter.date, 'long') }}</span>
-          <span v-if="frontmatter.duration">· {{ frontmatter.duration }}</span>
-        </div>
-        <hr class="mx-auto mt-8 prose">
-      </header> -->
       <article v-if="frontmatter.category && frontmatter.category.href === 'blogs'" class="m-auto prose text-lg max-w-screen-[920px] px-4 sm:px-6 lg:px-8 pb-12 lg:pb-24">
         <slot />
       </article>
       <article v-else class="m-auto prose">
         <slot />
       </article>
-      <!-- <ArticleRecommendations v-if="frontmatter.category && frontmatter.category.href === 'blogs'" :articles="blogs" /> -->
-      <!-- <ContactSection v-if="frontmatter.category && frontmatter.category.href === 'blogs'" /> -->
     </div>
-    <!-- but leave components as is -->
-
+    <!-- Legal -->
     <div v-if="frontmatter.category && frontmatter.category.href === 'legal'">
+      <article class="m-auto prose text-lg max-w-screen-xl px-4 sm:px-6 lg:px-8 pb-12 lg:pb-24">
+        <slot />
+      </article>
+    </div>
+    <!-- Jobs -->
+    <div v-if="frontmatter.category && frontmatter.category.href === 'jobs'">
+      <KarriereHeader :title="frontmatter.title" :period="frontmatter.period" />
       <article class="m-auto prose text-lg max-w-screen-xl px-4 sm:px-6 lg:px-8 pb-12 lg:pb-24">
         <slot />
       </article>
