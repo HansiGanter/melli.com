93328624c8ea60057de236f8fb9bebad808329dd
Hans Ganter <hansi.ganter@gmx.at>
2023-04-05 15:23:49 +0200

    fix contact support in 404

--- src/layouts/404.vue
+++ src/layouts/404.vue
@@ -17,9 +17,7 @@ const { t } = useI18n()
               </p>
               <div class="sm:ml-6">
                 <div class="sm:border-l sm:border-gray-200 sm:pl-6">
-                  <h1
-                    class="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl"
-                  >
+                  <h1 class="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
                     {{ t('404.not-found.heading') }}
                   </h1>
                   <p class="mt-1 text-base text-gray-500">
@@ -27,18 +25,14 @@ const { t } = useI18n()
                   </p>
                 </div>
                 <div class="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
-                  <router-link
-                    to="/"
-                    class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-primary-500 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
-                  >
+                  <router-link to="/"
+                    class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-primary-500 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500">
                     {{ t('404.go-home') }}
                   </router-link>
-                  <router-link
-                    to="/support"
-                    class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-primary-700 bg-primary-100 hover:bg-primary-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
-                  >
+                  <a href="tel:030555703440"
+                    class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-primary-700 bg-primary-100 hover:bg-primary-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500">
                     {{ t('404.contact-support') }}
-                  </router-link>
+                  </a>
                 </div>
               </div>
             </main>
