335bba5d56057c963ed517a9c253a3e7400c1e14
Hans Ganter <hansi.ganter@gmx.at>
2023-04-12 16:08:19 +0200

    update iframe for forms

--- src/components/Dipa/DipaTestUserForm.vue
+++ src/components/Dipa/DipaTestUserForm.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="h-80vh">
-    <iframe class="w-full h-full" src="https://ccfae1fd.sibforms.com/serve/MUIEADY9OLAvHaAw_XEPXQQtOEsJhmYFs-NSinQJhyLlT9NG0ECdC7MFlx4WehkhsCVvFlMMV15vlBkqINcpwEO6Uzd2LPD7u-JgZyGi2s8ykeBq-zG221G6KLX0bL01-iAk8bu7IbV1eLDbP4z5S1LmKs-3ZgbrBBYmabqrgf853JQuICxfxBYXTVcFnZ4KuGoHJXnGPAmb3__d" frameborder="0" scrolling="auto" allowfullscreen style="display: block;margin-left: auto;margin-right: auto;max-width: 100%;" />
-  </div>
-</template>
+    <iframe class="w-full h-full"
+      src="https://ccfae1fd.sibforms.com/serve/MUIEANLH6Q7NGPEZjWAd9iJoNpit9EwKw27zE41SVJD-lvobdvQRhlCVTTGuKV5fRPA_1CNXramrgTD-wdCvCCFYPRXMmqUFpwIZOATEQ2AZT_Kw5OHnXD4W08H66vVKWa83qlfQ9dzDoLH_WdIVAoFgmyYhvVXhL-T-ooXPjbpCFme7ZeozlOaLV-d7jZH8R1E6-sEU6pQxX2vK"
+      frameborder="0" scrolling="auto" allowfullscreen
+      style="display: block;margin-left: auto;margin-right: auto;max-width: 100%;" />
+  </div></template>
