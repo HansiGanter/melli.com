64e560c3e431e071627f720f404b60b8b375b3be
Hans Ganter <hansi.ganter@gmx.at>
2023-04-12 13:20:30 +0200

    add instagram to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -28,6 +28,11 @@ const navigation = {
       icon: 'i-carbon:logo-facebook h-5 w-5',
     },
     {
+      name: 'Instagram',
+      href: 'https://www.instagram.com/deine.melli/',
+      icon: 'i-carbon:logo-instagram h-5 w-5'
+    },
+    {
       name: 'LinkedIn',
       href: 'https://www.linkedin.com/company/mit-melli/',
       icon: 'i-carbon:logo-linkedin h-5 w-5',
@@ -49,29 +54,26 @@ const navigation = {
 <template>
   <footer aria-labelledby="footer-heading" class="py-12 lg:py-16">
     <div class="mb-12">
-      <hr>
-      <SocialMedia class="py-8" />
-      <hr>
-    </div>
-    <h2 id="footer-heading" class="sr-only">
-      Footer
-    </h2>
-    <div class="pb-8 grid grid-cols-1 xl:grid-cols-4 gap-8">
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-8 xl:col-span-4">
-        <div class="grid grid-cols-1 md:grid-cols-2 md:gap-8">
-          <div>
-            <h3
-              class="text-sm font-semibold text-primary-400 dark:text-white dark:text-white tracking-wider uppercase"
-            >
-              Sie brauchen Hilfe?
-            </h3>
-            <ul role="list" class="mt-4 space-y-2 flex-wrap">
-              <li class="text-lg font-semibold text-gray-500">
+    <hr>
+    <SocialMedia class="py-8" />
+    <hr>
+  </div>
+  <h2 id="footer-heading" class="sr-only">
+    Footer
+  </h2>
+  <div class="pb-8 grid grid-cols-1 xl:grid-cols-4 gap-8">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-8 xl:col-span-4">
+      <div class="grid grid-cols-1 md:grid-cols-2 md:gap-8">
+        <div>
+          <h3 class="text-sm font-semibold text-primary-400 dark:text-white dark:text-white tracking-wider uppercase">
+            Sie brauchen Hilfe?
+          </h3>
+          <ul role="list" class="mt-4 space-y-2 flex-wrap">
+            <li class="text-lg font-semibold text-gray-500">
                 Lassen Sie sich kostenlos beraten!
               </li>
               <li v-for="item in navigation.resources" :key="item.name">
-                <a
-                  :href="item.href"
+                <a :href="item.href"
                   class="text-gray-500 hover:text-gray-900 dark:text-white dark:hover:text-gray-300 flex gap-2 items-center"
                   @click="fireContactEvent(item.name)">
                   <div :class="item.icon" />
@@ -81,67 +83,54 @@ const navigation = {
             </ul>
           </div>
           <div class="mt-12 md:mt-0">
-            <h3
-              class="text-sm font-semibold text-primary-400 dark:text-white tracking-wider uppercase"
-            >
+            <h3 class="text-sm font-semibold text-primary-400 dark:text-white tracking-wider uppercase">
               FAQ & Login
             </h3>
             <ul role="list" class="mt-4 space-y-2">
               <li v-for="item in navigation.support" :key="item.name">
-                <a
-                  :href="item.href"
-                  class="text-gray-500 hover:text-gray-900 dark:text-white dark:hover:text-gray-300"
-                >{{ item.name }}</a>
+                <a :href="item.href" class="text-gray-500 hover:text-gray-900 dark:text-white dark:hover:text-gray-300">{{
+                  item.name }}</a>
               </li>
             </ul>
           </div>
         </div>
         <div class="grid grid-cols-1 md:grid-cols-2 md:gap-8">
           <div>
-            <h3
-              class="text-sm font-semibold text-primary-400 dark:text-white tracking-wider uppercase"
-            >
+            <h3 class="text-sm font-semibold text-primary-400 dark:text-white tracking-wider uppercase">
               Wichtige Links
             </h3>
             <ul role="list" class="mt-4 space-y-2">
               <li v-for="item in navigation.solutions" :key="item.name">
-                <a
-                  :href="item.href"
-                  class="text-gray-500 hover:text-gray-900 dark:text-white dark:hover:text-gray-300"
-                >{{ item.name }}</a>
+                <a :href="item.href" class="text-gray-500 hover:text-gray-900 dark:text-white dark:hover:text-gray-300">{{
+                  item.name }}</a>
               </li>
             </ul>
           </div>
           <!-- <div class="mt-12 md:mt-0">
-            <h3
-              class="text-sm font-semibold text-primary-400 dark:text-white tracking-wider uppercase"
-            >
-              Karriere
-            </h3>
-            <ul role="list" class="mt-4 space-y-2">
-              <li v-for="item in navigation.company" :key="item.name">
-                <router-link
-                  :to="item.href"
-                  class="text-gray-500 hover:text-gray-900 dark:text-white dark:hover:text-gray-300"
-                >
-                  {{ item.name }}
-                </router-link>
-              </li>
-            </ul>
-          </div> -->
+                  <h3
+                    class="text-sm font-semibold text-primary-400 dark:text-white tracking-wider uppercase"
+                  >
+                    Karriere
+                  </h3>
+                  <ul role="list" class="mt-4 space-y-2">
+                    <li v-for="item in navigation.company" :key="item.name">
+                      <router-link
+                        :to="item.href"
+                        class="text-gray-500 hover:text-gray-900 dark:text-white dark:hover:text-gray-300"
+                      >
+                        {{ item.name }}
+                      </router-link>
+                    </li>
+                  </ul>
+                </div> -->
           <div class=" lg:mt-0">
-            <h3
-              class="text-sm font-semibold text-primary-400 dark:text-white tracking-wider uppercase"
-            >
+            <h3 class="text-sm font-semibold text-primary-400 dark:text-white tracking-wider uppercase">
               Folge uns!
             </h3>
             <ul role="list" class="mt-4 space-y-2">
               <li v-for="item in navigation.social" :key="item.name" @click="fireContactEvent(item.name)">
-                <a
-                  :href="item.href"
-                  target="_blank"
-                  class="text-gray-500 hover:text-gray-900 dark:text-white dark:hover:text-gray-300 flex gap-2 items-center"
-                >
+                <a :href="item.href" target="_blank"
+                  class="text-gray-500 hover:text-gray-900 dark:text-white dark:hover:text-gray-300 flex gap-2 items-center">
                   <div :class="item.icon" />
                   <span>{{ item.name }}</span>
                 </a>
@@ -155,9 +144,7 @@ const navigation = {
       <Logo />
 
       <div class="flex gap-8 items-center">
-        <p
-          class="mt-8 text-gray-400 dark:text-white md:mt-0 md:order-1"
-        >
+        <p class="mt-8 text-gray-400 dark:text-white md:mt-0 md:order-1">
           &copy; {{ new Date().getFullYear() }} meetap, GmbH. All rights reserved.
         </p>
       </div>
