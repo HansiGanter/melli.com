91ad6967bddc8e479f6d32a923afcce3cefbffec
Hans Ganter <hansi.ganter@gmx.at>
2023-06-15 17:50:12 +0200

    refactor banner

--- src/components/DonationBanner.vue
+++ src/components/DonationBanner.vue
@@ -11,10 +11,12 @@ const showBanner = ref(true)
 </script>
 
 <template>
-  <div v-if="showBanner" class="flex p-2 bg-gray-100 rounded-b-2xl lg:rounded-b-3xl">
-    <RouterLink to="/shop" class="flex gap-2 lg:gap-4 mx-auto" @click="fireShopEvent">
-      <img src="https://assets.melli.com/bubble-icons/bubble-icon_heart_3.svg" class="h-12 lg:h-16 my-auto">
-      <div class="my-auto text-sm lg:text-base">
+  <div v-if="showBanner" class="flex p-2 bg-primary-700 rounded-b-2xl lg:rounded-b-3xl">
+    <RouterLink to="/shop" class="flex gap-3 lg:gap-4 mx-auto" @click="fireShopEvent">
+
+      <BubbleIcon :bubble-type="2" bubble="text-primary-900" icon="i-ph:heart w-12 h-12 sm:w-24 sm:h-24 text-#FACC15"
+        class="m-auto" />
+      <div class="my-auto text-sm lg:text-base text-white">
         <p v-if="heading" class="font-semibold">
           {{ t('group-session-banner.heading') }}
         </p>
@@ -23,6 +25,6 @@ const showBanner = ref(true)
         </p>
       </div>
     </RouterLink>
-    <button class="i-carbon:close w-8 h-8 my-auto p-2 lg:p-4" @click="showBanner = false" />
+    <button class="i-carbon:close w-8 h-8 my-auto p-2 lg:p-4 text-white" @click="showBanner = false" />
   </div>
 </template>
