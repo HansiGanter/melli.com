e792415a03c5fcd8404fb7a34bbc46979ea78c47
Hans Ganter <hansi.ganter@gmx.at>
2023-06-19 09:55:00 +0200

    fix banner

--- src/components/DonationBanner.vue
+++ src/components/DonationBanner.vue
@@ -12,9 +12,9 @@ const showBanner = ref(true)
 
 <template>
   <div v-if="showBanner" class="flex p-2 bg-primary-700 rounded-b-2xl lg:rounded-b-3xl">
-    <RouterLink to="/shop" class="flex gap-3 lg:gap-4 mx-auto" @click="fireShopEvent">
+    <RouterLink to="/shop" class="flex gap-3 lg:gap-4 mx-auto py-1" @click="fireShopEvent">
 
-      <BubbleIcon :bubble-type="2" bubble="text-primary-900" icon="i-ph:heart w-12 h-12 sm:w-24 sm:h-24 text-#FACC15"
+      <BubbleIcon :bubble-type="2" bubble="text-primary-900" icon="i-ph:heart w-12 h-12 sm:w-16 sm:h-16 text-#FACC15"
         class="m-auto" />
       <div class="my-auto text-sm lg:text-base text-white">
         <p v-if="heading" class="font-semibold">
