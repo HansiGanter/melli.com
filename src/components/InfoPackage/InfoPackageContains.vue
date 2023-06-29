e58f61f1e68b9a830b05bde837b7c8ee00e1f48d
Hans Ganter <hansi.ganter@gmx.at>
2023-06-29 16:34:43 +0200

    change infopaket image

--- src/components/InfoPackage/InfoPackageContains.vue
+++ src/components/InfoPackage/InfoPackageContains.vue
@@ -14,25 +14,25 @@ const benefits = ref([{ icon: 'i-heroicons:book-open', text: 'Melli-Informations
         <div class="grid gap-14">
           <div class="w-full lg:w-2/3">
             <p class="w-full lg:w-3/4 font-normal text-xl">
-              Wir haben dir eine Mappe mit allerhand Informationsmaterialien über Melli und unser Team zusammengestellt. Viel Spaß beim Stöbern!
+              Wir haben dir eine Mappe mit allerhand Informationsmaterialien über Melli und unser Team zusammengestellt.
+              Viel Spaß beim Stöbern!
             </p>
           </div>
         </div>
       </div>
 
       <div class="flex flex-wrap lg:flex-nowrap  gap-3">
-        <div
-          v-for="(benefit, index) in benefits"
-          :key="index"
-          class="text-gray-900 flex gap-2 px-4 py-2 rounded-xl w-full sm:w-70 bg-tertiary-800/10 lg:h-full  items-center"
-        >
+        <div v-for="(benefit, index) in benefits" :key="index"
+          class="text-gray-900 flex gap-2 px-4 py-2 rounded-xl w-full sm:w-70 bg-tertiary-800/10 lg:h-full  items-center">
           <div :class="benefit.icon" class="i-carbon:currency-dollar my-auto text-tertiary-500 h-12 w-12 shrink-0" />
           <span class="text-lg font-medium my-auto">{{ benefit.text }}</span>
         </div>
       </div>
     </div>
     <div class="relative overflow-hidden lg:max-w-6xl lg:-mt-88 mx-auto">
-      <img class="rounded-2xl mx-auto" src="https://assets.melli.com/images/stock/melli_mockup-print_flyer_businesscard-1024.webp" alt="Melli Magazin geöffnet auf Founder Seite">
+      <img class="rounded-2xl mx-auto"
+        src="https://assets.melli.com/images/stock/melli-mockup-print-flyer&businesscard-1024.webp"
+        alt="Melli Magazin geöffnet auf Founder Seite">
     </div>
   </div>
 </template>
