e0f2e7bd033a3d414519ba3cb26ab5cff4803837
Boemjay <benjamin.mollier@melli.com>
2023-07-28 15:39:02 +0200

    feat: keep search-params on route change for better tracking (#413)

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ViteSSG } from 'vite-ssg'
 import { setupLayouts } from 'virtual:generated-layouts'
 
 import type { RouterScrollBehavior } from 'vue-router'
+import { isNavigationFailure } from 'vue-router'
 import App from './App.vue'
 
 import type { UserModule } from './types'
@@ -30,6 +31,17 @@ export const createApp = ViteSSG(
   App,
   { routes, scrollBehavior, base: import.meta.env.BASE_URL },
   (ctx) => {
+    ctx.router.afterEach((to, from, failure) => {
+      if (isNavigationFailure(failure))
+        console.log('failed navigation', failure)
+
+      // keep search query params for tracking of UTM parameters
+      if (Object.entries(from.query).length > 0) {
+        to.query = from.query
+        const mergedParams = { ...to.query, ...from.query }
+        window.history.replaceState(to.path, '', `?${(new URLSearchParams(mergedParams)).toString()}`)
+      }
+    })
     // install all modules under `modules/`
     Object.values(import.meta.glob<{ install: UserModule }>('./modules/*.ts', { eager: true }))
       .forEach(i => i.install?.(ctx))
