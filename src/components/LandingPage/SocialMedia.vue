f274652fadaddbba8d60ff496f37909f06c37629
Serhii Sokoliuk <s.sokoliuk@gmail.com>
2023-08-14 10:17:46 +0200

    chore(social-media): Update insta and fb links (#415)

--- src/components/LandingPage/SocialMedia.vue
+++ src/components/LandingPage/SocialMedia.vue
@@ -11,9 +11,15 @@ import { fireSocialEvent } from '~/google-tag-manager'
       <a href="https://www.youtube.com/channel/UC9kINIc7RSUKfMu8LXaWnTQ" target="_blank">
         <div class="i-carbon:logo-youtube text-4xl text-gray-900" @click="fireSocialEvent('youtube')" />
       </a>
-      <a href="https://www.facebook.com/deine.melli" target="_blank"><div class="i-carbon:logo-facebook text-4xl text-gray-900" @click="fireSocialEvent('facebook')" /></a>
-      <a href="https://www.linkedin.com/company/mit-melli/" target="_blank"><div class="i-carbon:logo-linkedin text-4xl text-gray-900" @click="fireSocialEvent('linkedin')" /></a>
-      <a href="https://www.instagram.com/deine.melli/" target="_blank"><div class="i-carbon:logo-instagram text-4xl text-gray-900" @click="fireSocialEvent('linkedin')" /></a>
+      <a href="https://www.facebook.com/meine.melli" target="_blank">
+        <div class="i-carbon:logo-facebook text-4xl text-gray-900" @click="fireSocialEvent('facebook')" />
+      </a>
+      <a href="https://www.linkedin.com/company/mit-melli/" target="_blank">
+        <div class="i-carbon:logo-linkedin text-4xl text-gray-900" @click="fireSocialEvent('linkedin')" />
+      </a>
+      <a href="https://www.instagram.com/meine.melli/" target="_blank">
+        <div class="i-carbon:logo-instagram text-4xl text-gray-900" @click="fireSocialEvent('linkedin')" />
+      </a>
     </div>
   </div>
 </template>
