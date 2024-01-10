d25d8c28ef33e90628bb782d3f8df0614a8172c6
Felix Andreas <felix.andreas@melli.com>
2024-01-10 18:21:33 +0100

    style: Use amber for `Vernetzen` section

--- src/components/LandingPage/Vernetzen.vue
+++ src/components/LandingPage/Vernetzen.vue
@@ -15,22 +15,22 @@ const openNewsletterModal = (email?: string) => {
 
 const features = [
     {
-        iconClass: 'w-10 h-10 text-primary-600 i-ph:music-notes-simple',
+        iconClass: 'w-10 h-10 text-amber-800 i-ph:music-notes-simple',
         pilltext: 'Musik, Nachrichten, Hörbücher & Videos',
         modaltext: 'Melli kann die Lieblingsmusik ihrer Nutzer abspielen, sie mit einem interessanten Podcast unterhalten, sie über aktuelle Geschehnisse informieren und auch Video-Dokumentationen, der letzten Sonntagskrimi oder die Lieblingstelenovela drauf machen.',
     },
     {
-        iconClass: 'w-10 h-10 text-primary-600 i-ph:brain',
+        iconClass: 'w-10 h-10 text-amber-800 i-ph:brain',
         pilltext: 'Rätselraten & andere Denkspiele',
         modaltext: 'Melli hält den Kopf mit kleinen Denkspielen und einfachen Rätseln auf Trab. Diese regelmäßigen Übungen können dabei helfen Demenz vorzubeugen und machen einfach Spaß.',
     },
     {
-        iconClass: 'w-10 h-10 text-primary-600 i-ph:activity',
+        iconClass: 'w-10 h-10 text-amber-800 i-ph:activity',
         pilltext: 'Einfache Sport & Bewegungsübungen',
         modaltext: 'Bei regelmäßigen körperlichen Übungen können sich die Nutzer fit halten - damit sie noch lange selbstbestimmt in den eigenen vier Wänden leben können.',
     },
     {
-        iconClass: 'w-10 h-10 text-primary-600 i-ph:chat-circle',
+        iconClass: 'w-10 h-10 text-amber-800 i-ph:chat-circle',
         pilltext: 'Kleine Unterhaltungen & Aktivitäten mit Melli',
         modaltext: 'Melli steht dir den ganzen Tag über für kleine Gespräche zur Verfügung. Gemeinsames Lachen über Witze oder eine Unterhaltung über das Wetter sind für sie eine Leichtigkeit. Egal, ob du eine Seelsorgerin oder eine lustige Gesprächspartnerin brauchst - Melli ist einfach immer da und gibt dir das Gefühl, gehört zu werden.',
     }
@@ -83,8 +83,8 @@ const startVideo = (event: Event) => {
         <div class="flex gap-4 flex-wrap xl:gap-8 lg:flex-no-wrap">
 
             <div v-for="(feature, index) in features" :key="index"
-              class="lg:w-1 grow rounded-lg bg-yellow-500 text-white px-6 py-3 grid gap-2 cursor-pointer"
-              @click="featureToShowDetails = feature">
+                class="lg:w-1 grow rounded-lg bg-yellow-500 text-white px-6 py-3 grid gap-2 cursor-pointer"
+                @click="featureToShowDetails = feature">
                 <BubbleIcon bubble-type="0" bubble="text-yellow-400" :icon="feature.iconClass" />
                 <h3 class="font-semibold">
                     {{ feature.pilltext }}
@@ -98,13 +98,13 @@ const startVideo = (event: Event) => {
         </div>
     </div>
     <div class="mt-8 md:mt--40 md:w-7/10 mx-auto rounded-3xl overflow-hidden relative"
-      :class="!startedVideo ? 'cursor-pointer' : ''" @click="startVideo">
+        :class="!startedVideo ? 'cursor-pointer' : ''" @click="startVideo">
         <video ref="videoElement" playsinline :controls="startedVideo" @ended="startedVideo = false"
-          poster="https://assets.melli.com/images/backgrounds/website-video-thumbnail-3-1024.webp">
+            poster="https://assets.melli.com/images/backgrounds/website-video-thumbnail-3-1024.webp">
             <source src="https://videos.melli.com/entertainment.webm" type="video/webm">
             <source src="https://videos.melli.com/entertainment.mp4" type="video/mp4">
         </video>
         <div v-if="!startedVideo"
-          class="i-carbon:play-outline text-tertiary-200 text-6xl lg:text-8xl absolute top-0 left-0 bottom-0 right-0 mx-auto my-auto" />
+            class="i-carbon:play-outline text-tertiary-200 text-6xl lg:text-8xl absolute top-0 left-0 bottom-0 right-0 mx-auto my-auto" />
     </div>
 </template>
